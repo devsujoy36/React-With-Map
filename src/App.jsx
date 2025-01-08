a0dca12fc95d4564633c3526c3cf2a0948690e81
unknown <sujoydaspc2023@gmail.com>
2025-01-08 23:29:02 +0600

    adding map

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,32 @@
 
 import './App.css'
+import GoogleMapReact from 'google-map-react';
+import { googleAPIKey } from './googleAPIKey';
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 function App() {
-  
+  const defaultProps = {
+    center: {
+      lat: 10.99835602,
+      lng: 77.01502627
+    },
+    zoom: 11
+  };
   return (
     <>
-      <div className='flex justify-center items-center h-[90vh] font-bold text-4xl'>
-        <h1>Hello</h1>
-      </div>
+      <div style={{ height: '100vh', width: '100%' }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: googleAPIKey}}
+        defaultCenter={defaultProps.center}
+        defaultZoom={defaultProps.zoom}
+      >
+        <AnyReactComponent
+          lat={59.955413}
+          lng={30.337844}
+          text="My Marker"
+        />
+      </GoogleMapReact>
+    </div>
     </>
   )
 }
