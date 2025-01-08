a4d4a1a04ab46d541c5978b2885d0cdfc611c27c
unknown <sujoydaspc2023@gmail.com>
2025-01-08 23:03:20 +0600

    adding style in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   
   return (
     <>
-      <div>
+      <div className='flex justify-center items-center h-[90vh] font-bold text-4xl'>
         <h1>Hello</h1>
       </div>
     </>
