9ff02c28be388fffb8569bd2adba61a2b1a02ab3
unknown <sujoydaspc2023@gmail.com>
2025-01-09 22:27:45 +0600

    adding creator json

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 
 import './App.css'
 import GoogleMapReact from 'google-map-react';
@@ -15,18 +16,18 @@ function App() {
   return (
     <>
       <div style={{ height: '100vh', width: '100%' }}>
-      <GoogleMapReact
-        bootstrapURLKeys={{ key: googleAPIKey}}
-        defaultCenter={defaultProps.center}
-        defaultZoom={defaultProps.zoom}
-      >
-        <AnyReactComponent
-          lat={59.955413}
-          lng={30.337844}
-          text="My Marker"
-        />
-      </GoogleMapReact>
-    </div>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: googleAPIKey }}
+          defaultCenter={defaultProps.center}
+          defaultZoom={defaultProps.zoom}
+        >
+          <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text="My Marker"
+          />
+        </GoogleMapReact>
+      </div>
     </>
   )
 }
