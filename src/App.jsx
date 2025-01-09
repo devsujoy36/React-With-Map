76e9d779ac7392ca67081dad24eeb575be4d1679
unknown <sujoydaspc2023@gmail.com>
2025-01-09 23:44:17 +0600

    adding changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,7 @@ const App = () => {
   return (
     <div>
       <h1>Hello</h1>
-      <div>
         <Outlet />
-      </div>
     </div>
   )
 }
--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -6,29 +6,32 @@ import { googleAPIKey } from './googleAPIKey';
 const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
 function Map() {
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627
-    },
-    zoom: 11
-  };
-  return (
-    
-      <div style={{ height: '100vh', width: '100%' }}>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: googleAPIKey }}
-          defaultCenter={defaultProps.center}
-          defaultZoom={defaultProps.zoom}
-        >
-          <AnyReactComponent
-            lat={59.955413}
-            lng={30.337844}
-            text="My Marker"
-          />
-        </GoogleMapReact>
-      </div>
-  )
+    const defaultProps = {
+        center: {
+            lat: 10.99835602,
+            lng: 77.01502627
+        },
+        zoom: 11
+    };
+    return (
+        <div className='max-w-screen-2xl mx-auto flex justify-center items-center min-h-[90vh]'>
+            <div className=''>
+                <div style={{ height: '50vh', width: '50vw' }}>
+                    <GoogleMapReact
+                        bootstrapURLKeys={{ key: googleAPIKey }}
+                        defaultCenter={defaultProps.center}
+                        defaultZoom={defaultProps.zoom}
+                    >
+                        <AnyReactComponent
+                            lat={59.955413}
+                            lng={30.337844}
+                            text="My Marker"
+                        />
+                    </GoogleMapReact>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Map
