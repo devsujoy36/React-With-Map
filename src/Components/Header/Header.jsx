84ca2aabca53aeaf0a6c48f6766d7ce949efffd2
unknown <sujoydaspc2023@gmail.com>
2025-01-10 23:41:53 +0600

    style nav

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className="py-5 flex justify-between items-center">
         <div><h1 className="font-medium text-4xl">React-Map-Creator</h1></div>
-        <div className="flex justify-center items-center gap-10 text-xl font-medium">
+        <div className="flex justify-center items-center md:gap-20 gap-5 text-xl font-medium">
             <NavLink to={"/"}>Home</NavLink>
             <NavLink to={"/creators"}>Creator</NavLink>
             <NavLink to={"/map"}>Map</NavLink>
