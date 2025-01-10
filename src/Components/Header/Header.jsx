1b1c99e71275f8130945883223fb1ae76341d75f
unknown <sujoydaspc2023@gmail.com>
2025-01-10 22:37:39 +0600

    adding header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,11 +3,11 @@ import { NavLink } from "react-router-dom"
 
 const Header = () => {
   return (
-    <div>
+    <div className="max-w-screen-2xl md:mx-auto mx-10 py-5 flex justify-between items-center">
         <div><h1>React-Map-Creator</h1></div>
-        <div>
+        <div className="flex justify-center items-center gap-2">
             <NavLink to={"/"}>Home</NavLink>
-            <NavLink to={"/creator"}>Creator</NavLink>
+            <NavLink to={"/creators"}>Creator</NavLink>
             <NavLink to={"/map"}>Map</NavLink>
             <NavLink to={"/"}>Contact</NavLink>
         </div>
