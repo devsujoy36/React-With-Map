c7bfca58b3ba998ade121bc2ad4b5be818de231b
unknown <sujoydaspc2023@gmail.com>
2025-01-11 23:42:41 +0600

    change in creator

--- src/Components/Creators/Creator.jsx
+++ src/Components/Creators/Creator.jsx
@@ -1,11 +1,16 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
 
-const Creator = ({creator}) => {
+const Creator = ({ creator }) => {
     console.log(creator);
-    
+    const { id, name, img, alias, profession, date_of_birth, nationality, notable_for, social_media, businesses, philanthropy,achievements } = creator;
 
-  return (
-    <div></div>
-  )
+    const {youtube,twitter,instagram} = social_media;
+
+
+    return (
+        <div></div>
+    )
 }
 
 export default Creator
